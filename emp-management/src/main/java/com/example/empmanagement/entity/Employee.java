60126883548c0e8e7ced659e8dfc49f42e99a7f2
Computers <ashutoshkmrsingh380@gmail.com>
2024-01-24 12:19:46 +0530

    added employee entity

---
+++ emp-management/src/main/java/com/example/empmanagement/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.example.empmanagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String dept;
+    private double salary;
+
+//todo: add validation
+}
